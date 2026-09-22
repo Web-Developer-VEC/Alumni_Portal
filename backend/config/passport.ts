7fc0cdfcdb9ee8705d0521439a2c1a31dfd25fa3
Priyadharsan T <priyadharsant4@gmail.com>
2026-09-22 14:19:25 +0530

    passport upd

--- backend/config/passport.ts
+++ backend/config/passport.ts
@@ -5,9 +5,9 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+      clientID: process.env.GOOGLE_CLIENT_ID || "dummy_google_client_id",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "dummy_google_client_secret",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:5000/api/auth/google/callback",
     },
 
     async (accessToken, refreshToken, profile, done) => {
