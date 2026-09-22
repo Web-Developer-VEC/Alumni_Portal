93874bb9a55d0333a1cd3a8da09900efa92e0b23
uhemandkumaar-debug <u.hemandkumaar@gmail.com>
2026-09-22 12:10:46 +0530

    config

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,23 @@
-import express, { type Request,type Response } from "express";
+import express, { type Request, type Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
