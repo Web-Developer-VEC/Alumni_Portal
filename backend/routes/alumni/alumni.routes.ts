82fd462a02244e561e3c1ab4ced64df88e1ed6c8
uhemandkumaar-debug <u.hemandkumaar@gmail.com>
2026-09-24 11:18:08 +0530

    get alumni details

--- backend/routes/alumni/alumni.routes.ts
+++ backend/routes/alumni/alumni.routes.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import { completeProfile } from "../../controllers/alumni/alumni.controller.js";
+import {
+  getAllAlumniDetails,
+  getAlumniById,
+} from "../../controllers/alumni/getalumnidetails.controller.js";
+
 const router = express.Router();
 
-
 router.post("/complete-profile", completeProfile);
+
+router.get("/details", getAllAlumniDetails);
+router.get("/:id", getAlumniById);
 
 export default router;
--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,7 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/auth/auth.route.js";
+
 import passport from "./config/passport.js";
 import session from "express-session";
 import router from "./routes/index.routes.js";
