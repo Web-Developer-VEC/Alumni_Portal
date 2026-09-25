0dae4a087ef826ec3a71f7fd81b22e9feb4fbd6c
MANOJKUMAR A <amanojkumar991@gmail.com>
2026-09-25 19:22:11 +0530

    err fix

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,7 +4,6 @@ import LandingPage from "../pages/Landing/LandingPage";
 import Login from "../pages/auth/Login";
 import AlumniJobFeed from "../pages/posts/PostDetails";
 
-<<<<<<< Updated upstream
 import AlumniApproval from "../pages/admin/AlumniApproval";
 
 import AdminLayout from "../layouts/adminLayout";
@@ -12,12 +11,12 @@ import AlumniLayout from "../layouts/AlumniLayout";
 
 import Members from "../pages/alumni/Members"
 
-import FeedbackForm from "../pages/feedback/FeedbackForm"; 
+// import FeedbackForm from "../pages/feedback/FeedbackForm"; 
 function AppRoute() {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
-
+            <Route path="/" element={<LandingPage />} />
             <Route path="/jobs" element={<AlumniJobFeed />} />
 
             <Route path="/admin" element={<AdminLayout />}>
@@ -26,19 +25,9 @@ function AppRoute() {
             <Route path="/alumni" element={<AlumniLayout/>}>
                 <Route path="members" element={<Members />} />
             </Route>
-            <Route path="/feedback" element={<FeedbackForm />} />
+            {/* <Route path="/feedback" element={<FeedbackForm />} /> */}
         </Routes>
     );
-=======
-function AppRoute() {
-  return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/jobs" element={<AlumniJobFeed />} />
-    </Routes>
-  );
->>>>>>> Stashed changes
 }
 
 export default AppRoute;
